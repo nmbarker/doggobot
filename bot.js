e714c51d5ed959723b09f90e2996f23cc69b75a2
nmbarker <nickbarker3940@gmail.com>
2017-06-23 01:07:00 -0400

    scrapped, new take

--- bot.js
+++ bot.js
@@ -1,27 +1,20 @@
 //constants
 const Discord = require("discord.js");
-const client = new Discord.Client();
-const prefix = "!"
+const prefix = "foo "
+const token = 'MzI2MTgwMDEyMzg4NTE1ODQx.DCjCaw.IYndOdgU18b9GgfdAtXJqDEpgm0';
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+var bot = new Discord.Client();
+
+bot.on("ready", function() {
+  console.log(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.on("message", function(message) {
+  if (message.author.equals(bot.user)) return;
+  if (message.content == prefix + "ping") {
+    message.channel.send("pong");
+  }
 });
 
 
-client.on('message', msg => {
-  if (msg.content === prefix + 'ping') {
-    msg.channel.send('Pong!');
-  }
-  if (msg.content === prefix + 'help') {
-    msg.channel.send('Commands: !ping, !help, !rules, !bios, !hack, !format');
-  }
-  if (msg.content === prefix + 'bios') {
-    msg.channel.send('To post a bio and only a bio, go to the oc-bios chat and follow the format. For the format style use the !format command.');
-  }
-  if (msg.content === prefix + 'format') {
-    msg.channel.send('Bio Must Include:\
-    Name, Nickname, Age, Height, Gender, Race, Appearence, Backstory/Info, RP Chat')
-  }
-});
-
-client.login('MzI2MTgwMDEyMzg4NTE1ODQx.DCjCaw.IYndOdgU18b9GgfdAtXJqDEpgm0');
+bot.login(token);
